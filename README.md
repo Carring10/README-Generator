1990a75164b94a0975af09ddd0144c1b08f3eb11
Carrington <hartis.c99@gmail.com>
2022-03-16 18:14:32 -0400

    readme generator is working

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# README-Generator
+# TITLE
+
+  ## Description
+
+  it is very nice 
+
+  ## Table of Contents
+
+  here
+
+  ## Installation
+
+   just click it 
+
+  ## Instructions for Use
+
+   click IT
+
+  
---
+++ index.js
@@ -0,0 +1,73 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Provide a short description explaining your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Add a table of contents.",
+      name: "tableOfContents",
+    },
+    {
+      type: "input",
+      message: "What are the steps to install your project?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Provide instructions for use.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "License for Use.",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Provide guidelines for contributing",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Write tests for your application.",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "FAQ",
+      name: "questions",
+    },
+  ])
+  .then((userInput) => {
+    console.log(userInput);
+    writeToFile("README.md", userInput);
+  })
+  .catch((err) => console.log(err));
+
+// TODO: Create a function to write README file
+
+function writeToFile(fileName, data) {
+  console.log("name", data.name);
+  fs.writeFile(fileName, generateMarkdown(data), (err) => {
+    err ? console.error(err) : console.log("Success!");
+  });
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
